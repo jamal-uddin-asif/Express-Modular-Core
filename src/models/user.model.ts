0b9ea7b5e5a002751dd21f7e2972ac6d73969ea2
jamal-uddin-asif <asifzehendmg@gmail.com>
2026-08-05 18:23:58 +0600

    Added: User Schema

---
+++ src/models/user.model.ts
@@ -0,0 +1,16 @@
+import { model, Schema } from "mongoose";
+import { TUser } from "../types/user.interface";
+
+const itemSchema = new Schema<TUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
+ 
+},
+{
+  timestamps: true
+}
+);
+
+export const Item = model<TUser>("Users", itemSchema);
